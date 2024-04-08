9231c0634bd57ba865b0dd34d20715e724e18f7a
Roshanbnair <roshanbn20@gmail.com>
2024-04-08 10:55:09 +0530

    HOME changed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -40,7 +40,7 @@ function Navbar() {
                 className="nav-links"
                 onClick={handleClick}
               >
-                Home
+                HOME
               </Link>
             </li>
             <li className="nav-item">
